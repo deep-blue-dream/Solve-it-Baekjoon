3a86c57f8baee7384a64043dc6f488c30ce8d409
tutleCord <102152693+deep-blue-dream@users.noreply.github.com>
2022-07-26 22:01:56 +0900

    [Bronze V] Title: X보다 작은 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,2 @@
 n, x = map(int,input().split())                  # n은 수열을 이루는 정수의 개수, x는 비교할 정수
-A = list(map(int, input().split()))              # 두 번째 입력받은 값을 이용해서 리스트(수열)를 만든다.
-
-for i in range(n):                               #리스트 수 = n번만큼 반복한다
-    if A[i] < x:                                 #리스트 A[i]값이 x보다 작다면
-        print(A[i], end=" ")                     # 그 값을 출력한다, 쉼표로 구분해서 출력
+print(*(i for i in list(map(int, input().split())) if x > i))
